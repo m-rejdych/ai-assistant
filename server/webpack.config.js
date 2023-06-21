7f785228c7b7274b5ef795a8ab2e4cf1dd13ebea
m-rejdych <rejdych.m.r@gmail.com>
2023-06-21 23:33:36 +0200

    Added server configuration.

---
+++ server/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'build.[contenthash:8].js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  target: 'node',
+  node: false,
+  mode: 'production',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /.ts$/,
+        use: ['babel-loader', 'ts-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
