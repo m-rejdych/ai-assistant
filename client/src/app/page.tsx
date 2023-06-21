a51a187232e74954e476a2a4201e52bb6722dde2
m-rejdych <rejdych.m.r@gmail.com>
2023-06-21 23:15:03 +0200

    Changed to monorepo.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,24 +1,37 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
 import { register, isRegistered } from '@tauri-apps/api/globalShortcut';
 
 export default function Home() {
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+
   useEffect(() => {
+    const focusInput = (): void => {
+      if (!inputRef.current) return;
+
+      inputRef.current.focus();
+    };
+
+    window.addEventListener('focus', focusInput);
+
     (async () => {
       await invoke('resize_window');
       if (!(await isRegistered('Alt+Shift+Ctrl+Cmd+A'))) {
         await register('Alt+Shift+Ctrl+Cmd+A', () => invoke('toggle_window'));
       }
-
     })();
+
+    return () => {
+      window.removeEventListener('focus', focusInput);
+      };
   }, []);
 
   return (
     <main className="h-full px-4 py-8 flex flex-col">
       <div className="flex-1" />
-      <textarea className="textarea textarea-primary resize-none"></textarea>
+      <textarea className="textarea textarea-primary resize-none" ref={inputRef}></textarea>
     </main>
   );
 }
