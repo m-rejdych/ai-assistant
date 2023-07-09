3c226f02e61b2b1ec61d07d07cf02901c9f41959
m-rejdych <rejdych.m.r@gmail.com>
2023-07-09 15:16:28 +0200

    Moved initial setup to layout.

--- client/src/app/(chat)/layout.tsx
+++ client/src/app/(chat)/layout.tsx
@@ -3,17 +3,66 @@
 import { useEffect, useState } from 'react';
 import { useRouter, useParams } from 'next/navigation';
 import { invoke } from '@tauri-apps/api/tauri';
-import { useAtomValue } from 'jotai';
+import { isRegistered, register } from '@tauri-apps/api/globalShortcut';
+import { useAtom } from 'jotai';
 import { MdHistory, MdAdd } from 'react-icons/md';
 
 import { hasApiKeyAtom } from '../../atoms/apiKey';
 import type { Chat } from '../../types/chat';
 
+const HOTKEY = 'Alt+Shift+Ctrl+A' as const;
+const HOTKEY_RESIZE = 'Alt+Shift+Ctrl+S' as const;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   const [chats, setChats] = useState<Chat[]>([]);
-  const hasApiKey = useAtomValue(hasApiKeyAtom);
+  const [hasApiKey, setHasApiKey] = useAtom(hasApiKeyAtom);
+  const [isInit, setIsInit] = useState(false);
   const router = useRouter();
   const { chatId } = useParams();
+
+  useEffect(() => {
+    const initialSetup = async (): Promise<void> => {
+      try {
+        await invoke('resize_window');
+        if (!(await isRegistered(HOTKEY))) {
+          await register(HOTKEY, async () => invoke('toggle_window'));
+        }
+        if (!(await isRegistered(HOTKEY_RESIZE))) {
+          await register(HOTKEY_RESIZE, async () => await invoke('resize_window'));
+        }
+
+        const hasKey = await invoke<boolean>('has_api_key');
+
+        if (hasKey) {
+          const isValid = await invoke<boolean>('validate_stored_api_key');
+          if (!isValid) {
+            await invoke('clear_api_key');
+            setHasApiKey(false);
+            //          else if (params.get('skipActiveChatCheck') !== 'true') {
+            //            const activeChatId = await invoke<string>('get_active_chat');
+            //            if (activeChatId) {
+            //              router.push(`/${activeChatId}`);
+            //              setHasApiKey(true);
+            //              return;
+            //            }
+            //
+            //            setHasApiKey(true);
+            //          } else {
+            //            setHasApiKey(true);
+            //          }
+          } else {
+            setHasApiKey(true);
+          }
+        } else {
+          setHasApiKey(false);
+        }
+
+        setIsInit(true);
+      } catch { }
+    };
+
+    initialSetup();
+  }, []);
 
   useEffect(() => {
     (async () => {
@@ -27,6 +76,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     router.push(`/${selectedId}`);
   };
 
+  if (!isInit) return null;
+
   if (!hasApiKey) return (
     <div className="overflow-hidden h-full px-4 py-8">
       {children}
@@ -39,18 +90,22 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="drawer-content overflow-hidden h-full px-4 py-8">
         {children}
         <div className="fixed top-3 left-3 flex flex-col items-center">
-          <label htmlFor="chats" className="btn btn-sm btn-square drawer-button">
-            <MdHistory className="text-xl" />
-          </label>
-          <button className="btn btn-sm btn-square mt-2" onClick={() => router.push('/?skipActiveChatCheck=true')}>
-            <MdAdd className="text-xl" />
-          </button>
+          <div className="tooltip tooltip-right" data-tip="Chat history">
+            <label htmlFor="chats" className="btn btn-sm btn-square drawer-button">
+              <MdHistory className="text-xl" />
+            </label>
+          </div>
+          <div className="tooltip tooltip-right" data-tip="New chat">
+            <button className="btn btn-sm btn-square mt-2" onClick={() => router.push('/?skipActiveChatCheck=true')}>
+              <MdAdd className="text-xl" />
+            </button>
+          </div>
         </div>
       </div>
       <div className="drawer-side">
         <label htmlFor="chats" className="drawer-overlay" />
         <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
-          <li className="menu-title text-lg text-center">History</li>
+          <li className="menu-title text-lg text-center">Chat history</li>
           {chats.map(({ id, name }) => (
             <li key={id} className="w-full overflow-x-hidden" onClick={() => handleSelect(id)}>
               <a className={id === chatId ? 'active' : undefined}>{name}</a>
--- client/src/app/(chat)/page.tsx
+++ client/src/app/(chat)/page.tsx
@@ -3,18 +3,13 @@
 import { useEffect, useRef, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { invoke } from '@tauri-apps/api/tauri';
-import { register, isRegistered } from '@tauri-apps/api/globalShortcut';
 import { useAtom } from 'jotai';
 
 import { Chat, ApiKeyInput, PromptTextarea } from '../../components/chat';
 import { hasApiKeyAtom } from '../../atoms/apiKey';
 import type { SendMessageData } from '../../types/chat';
 
-const HOTKEY = 'Alt+Shift+Ctrl+A' as const;
-const HOTKEY_RESIZE = 'Alt+Shift+Ctrl+S' as const;
-
 export default function Home() {
-  const [isInit, setIsInit] = useState(false);
   const [hasApiKey, setHasApiKey] = useAtom(hasApiKeyAtom);
   const [pendingPrompt, setPendingPrompt] = useState('');
   const router = useRouter();
@@ -28,44 +23,16 @@ export default function Home() {
       else if (apiKeyRef.current) apiKeyRef.current.focus();
     };
 
-    const initialSetup = async () => {
-      try {
-        await invoke('resize_window');
-        if (!(await isRegistered(HOTKEY))) {
-          await register(HOTKEY, async () => invoke('toggle_window'));
-        }
-        if (!(await isRegistered(HOTKEY_RESIZE))) {
-          await register(HOTKEY_RESIZE, async () => await invoke('resize_window'));
-        }
+    const findActiveChat = async (): Promise<void> => {
+      if (params.get('skipActiveChatCheck') === 'true') return;
 
-        const hasApiKey = await invoke<boolean>('has_api_key');
-
-        if (hasApiKey) {
-          const isValid = await invoke<boolean>('validate_stored_api_key');
-          if (!isValid) {
-            await invoke('clear_api_key');
-            setHasApiKey(false);
-          } else if (params.get('skipActiveChatCheck') !== 'true') {
-            const activeChatId = await invoke<string>('get_active_chat');
-            if (activeChatId) {
-              router.push(`/${activeChatId}`);
-              setHasApiKey(true);
-              return;
-            }
-
-            setHasApiKey(true);
-          } else {
-            setHasApiKey(true);
-          }
-        } else {
-          setHasApiKey(false);
-        }
-
-        setIsInit(true);
-      } catch { }
+      const activeChatId = await invoke<string>('get_active_chat');
+      if (activeChatId) {
+        router.push(`/${activeChatId}`);
+      }
     };
 
-    initialSetup();
+    findActiveChat();
 
     window.addEventListener('focus', focusInput);
 
@@ -88,8 +55,6 @@ export default function Home() {
       } catch { }
     }
   };
-
-  if (!isInit) return null;
 
   return (
     <main className="h-full flex flex-col overflow-hidden">
